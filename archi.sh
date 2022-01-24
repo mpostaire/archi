29ac755eb7a5b80093c481cfbc1d661c3627efdb
mpostaire <maxime_postaire@hotmail.fr>
2022-01-24 18:04:58 +0100

    refactoring

--- archi.sh
+++ archi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Thanks to https://github.com/classy-giraffe/easy-arch
+# Thanks to https://github.com/classy-giraffe/easy-arch and https://github.com/helmuthdu/aui/
 
 set -eu
 shopt -s extglob
@@ -22,6 +22,66 @@ next() {
 "
 }
 
+# arguments: '-s': don't show input (useful for passwords); '-e': allow empty input.
+# returns result in 'ret' variable
+read_input() {
+    _secret=0
+    _allow_empty=0
+    for arg in "$@"; do
+        [ "$arg" = "-s" ] && _secret=1
+        [ "$arg" = "-e" ] && _allow_empty=1
+    done
+
+    while true; do
+        if [ $_secret -eq 1 ]; then
+            read -rs ret
+        else
+            read -r ret
+        fi
+        case $ret in
+            "" )
+                if [ $_allow_empty -eq 1 ]; then
+                    return
+                fi;;
+            *\ * ) ;;
+            * ) return;;
+        esac
+        printf "Invalid input.\n\n> "
+    done
+}
+
+# first argument is the prompt, second argument is the newline ('\n') separated choices
+# returns result in 'ret' variable
+choose() {
+    mapfile -t choices < <(echo -e "$2")
+    to_show=$(echo -e "$2" | nl -s ') ')
+
+    [ ${#choices[@]} -eq 0 ] && return 1
+    [ ${#choices[@]} -eq 1 ] && ret=${choices[0]} && return
+
+    while true; do
+        printf "%s (leave blank for '%s'):\n%s\n> " "$1" "${choices[0]}" "$to_show"
+        read_input -e
+
+        case $ret in
+            "" ) ret=${choices[0]}; return;;
+            +([0-9]) )
+                ret=$((ret -= 1))
+                if ((ret >= 0 && ret < ${#choices[@]})); then
+                    ret=${choices[$ret]}
+                    return
+                fi;;
+            * )
+                for elem in "${choices[@]}"; do
+                    if [ "$elem" = "$ret" ]; then
+                        return
+                    fi
+                done;;
+        esac
+        printf "Invalid input\n\n"
+    done
+}
+
 show_drives() {
     lsblk -po NAME,RM,SIZE,RO,TYPE,PTTYPE,FSTYPE,MOUNTPOINTS
 }
@@ -34,17 +94,10 @@ detect_efi() {
     fi
 }
 
-select_keyboard_layout() {
-    while true; do
-        printf "Select the keyboard layout:\n\t1) fr-latin9\n\t2) be-latin1\n\t3) us\n> "
-        read -r kbd
-        case $kbd in
-            1|fr-latin9 ) loadkeys fr-latin9; break;;
-            2|be-latin1 ) loadkeys be-latin1; break;;
-            3|us ) break;;
-            * ) printf "Invalid input.\n\n";;
-        esac
-    done
+ask_keyboard_layout() {
+    choose "Select the keyboard layout" "fr-latin9\nbe-latin1\nus"
+    kbd="$ret"
+    loadkeys "$kbd"
 }
 
 update_system_clock() {
@@ -55,11 +108,11 @@ partition_drives() {
     while true; do
         show_drives
         printf "\nEnter a drive to partition ('/dev/sda' for example) and type 'done' when there is nothing else to do:\n> "
-        read -r drive
-        case $drive in
+        read_input
+        case $ret in
             done ) break;;
             * )
-                if ! cfdisk "$drive"; then
+                if ! cfdisk "$ret"; then
                     printf "Invalid input.\n\n"
                 fi
                 next;;
@@ -71,11 +124,11 @@ format_partitions() {
     while true; do
         show_drives
         printf "\nEnter a partition to format ('/dev/sda1' for example) and type 'done' when there is nothing else to do:\n> "
-        read -r part
-        case $part in
+        read_input
+        case $ret in
             done ) break;;
             * )
-                if ! mkfs.ext4 "$part"; then
+                if ! mkfs.ext4 "$ret"; then
                     printf "Invalid input.\n\n"
                 fi
                 sleep 1;; # give time for lsblk to show updated fs
@@ -89,8 +142,8 @@ mount_filesystems() {
     while true; do
         show_drives
         printf "\nEnter the partition to use as root volume ('/dev/sda1' for example):\n> "
-        read -r part
-        if mount "$part" /mnt; then
+        read_input
+        if mount "$ret" /mnt; then
             break
         fi
         printf "Invalid input.\n\n"
@@ -101,14 +154,15 @@ mount_filesystems() {
     while true; do
         show_drives
         printf "\nEnter a mountpoint ('/home' for example) or type 'done' if there is nothing else to do:\n> "
-        read -r mountpoint
-        case $mountpoint in
+        read_input
+        mountpoint=$ret
+        case $ret in
             done ) break;;
             /* )
                 mkdir -p /mnt"$mountpoint"
                 printf "Select the partition to mount for '%s' ('/dev/sda2' for example):\n> " "$mountpoint"
-                read -r part
-                if ! mount "$part" /mnt"$mountpoint"; then
+                read_input
+                if ! mount "$ret" /mnt"$mountpoint"; then
                     printf "Invalid input.\n\n"
                 else
                     printf "\n"
@@ -118,22 +172,77 @@ mount_filesystems() {
     done
 }
 
+ask_grub() {
+    choose "Select the drive where GRUB will be installed" "$(lsblk -dpn -o NAME)"
+    grub_drive="$ret"
+}
+
 setup_swapfile() {
     while true; do
         printf "\nEnter the swapfile size in MiB (0 = no swap):\n> "
-        read -r ssize
-        case $ssize in
+        read_input
+        case $ret in
             +(0) ) break;;
             +([0-9]) )
-                if dd if=/dev/zero of=/mnt/swapfile bs=1M count="$ssize" status=progress; then
+                if dd if=/dev/zero of=/mnt/swapfile bs=1M count="$ret" status=progress; then
                     chmod 600 /mnt/swapfile
                     mkswap /mnt/swapfile
                     # swapon??
                     break
                 fi;;
-            * ) printf "Invalid input.\n\n";;
+            * ) printf "Invalid input\n\n";;
         esac
     done
+}
+
+ask_root_password() {
+    while true; do
+        printf "\nEnter the root password:\n> "
+        read_input -s
+        rootpasswd=$ret
+
+        printf "\nEnter the root password again:\n> "
+        read_input -s
+        if [ "$rootpasswd" = "$ret" ]; then
+            printf "\n"
+            break;
+        else
+            printf "The passwords dont match! Try again\n"
+        fi
+    done
+}
+
+ask_username_and_password() {
+    while true; do
+        printf "\nEnter the new username:\n> "
+        read_input
+        user=$ret
+        if arch-chroot /mnt useradd -m -G wheel -s /bin/zsh "$user"; then
+            break
+        else
+            printf "Invalid input\n\n"
+        fi
+    done
+
+    while true; do
+        printf "\nEnter the password for '%s':\n> " "$user"
+        read_input -s
+        userpasswd=$ret
+
+        printf "\nEnter the user password for '%s' again:\n> " "$user"
+        read_input -s
+        if [ "$userpasswd" = "$ret" ]; then
+            break
+        else
+            printf "The passwords dont match! Try again\n"
+        fi
+    done
+}
+
+ask_hostname() {
+    printf "\nEnter the hostname ('arch-laptop' for example):\n> "
+    read_input
+    hostname=$ret
 }
 
 detect_microcode() {
@@ -202,21 +311,14 @@ install_base() {
 }
 
 install_grub() {
-    while true; do
-        show_drives
-        printf "\nEnter the drive where grub will be installed ('/dev/sda' for example):\n> "
-        read -r drive
-        if arch-chroot /mnt grub-install --target=i386-pc "$drive"; then
-            sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /mnt/etc/default/grub
-            printf "\nEdit grub config? [y/N]:\n> "
-            read -r sel
-            case $sel in
-                y|Y ) nano /mnt/etc/default/grub;;
-            esac
-            arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-            break;
-        fi
-    done
+    arch-chroot /mnt grub-install --target=i386-pc "$grub_drive"
+    sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /mnt/etc/default/grub
+    printf "\nEdit grub config? [y/N]:\n> "
+    read_input -e
+    case $ret in
+        y|Y ) nano /mnt/etc/default/grub;;
+    esac
+    arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 set_shell_timezone_clock_locales() {
@@ -241,72 +343,11 @@ set_shell_timezone_clock_locales() {
 EOF
 }
 
-ask_hostname() {
-    while true; do
-        printf "\nEnter the hostname ('arch-laptop' for example):\n> "
-        read -r hostname
-        case $hostname in
-            "" ) printf "Invalid input.\n\n";;
-            * )
-                printf "%s" "$hostname" > /mnt/etc/hostname
-                printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t%s.localdomain\t%s" "$hostname" "$hostname" >> /mnt/etc/hosts
-                break;;
-        esac
-    done
-}
-
-ask_root_passwd() {
-    while true; do
-        while true; do
-            printf "\nEnter the root password:\n> "
-            read -rs rootpasswd
-            case $rootpasswd in
-                "" ) printf "Invalid input.\n\n";;
-                * ) break;;
-            esac
-        done
+set_hostname_user_and_passwords() {
+    printf "%s" "$hostname" > /mnt/etc/hostname
+    printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t%s.localdomain\t%s" "$hostname" "$hostname" >> /mnt/etc/hosts
     
-        printf "\nEnter the root password again:\n> "
-        read -rs rootpasswd2
-        if [ "$rootpasswd" = "$rootpasswd2" ]; then
-            printf "\n"
-            break;
-        else
-            printf "The passwords dont match! Try again\n"
-        fi
-    done
-
     printf "root:%s" "$rootpasswd" | arch-chroot /mnt chpasswd
-}
-
-create_user() {
-    while true; do
-        printf "\nEnter the new username:\n> "
-        read -r user
-        case $user in
-            "" ) printf "Invalid input.\n\n";;
-            * ) arch-chroot /mnt useradd -m -G wheel -s /bin/zsh "$user"; break;;
-        esac
-    done
-
-    while true; do
-        while true; do
-            printf "\nEnter the password for '%s':\n> " "$user"
-            read -rs userpasswd
-            case $userpasswd in
-                "" ) printf "Invalid input.\n\n";;
-                * ) break;;
-            esac
-        done
-    
-        printf "\nEnter the user password for '%s' again:\n> " "$user"
-        read -rs userpasswd2
-        if [ "$userpasswd" = "$userpasswd2" ]; then
-            break
-        else
-            printf "The passwords dont match! Try again\n"
-        fi
-    done
 
     printf "%s:%s" "$user" "$userpasswd" | arch-chroot /mnt chpasswd
     sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /mnt/etc/sudoers
@@ -314,25 +355,27 @@ create_user() {
 
 install_preset() {
     printf "Install the Gnome preset? [Y/n]:\n> "
-    read -r sel
-    case $sel in
+    read_input -e
+    case $ret in
         n|N ) return;;
     esac
 
     vdriver=""
     while [ "$hypervisor" = "none" ]; do
         printf "\nSelect the video driver to install:\n\t1) xf86-video-amdgpu (NEW)\n\t2) xf86-video-ati (OLD)\n\t3) xf86-video-intel\n\t4) nvidia\n\n> "
-        read -r vdriver
+        read_input
+        vdriver=$ret
         case $vdriver in
             1|xf86-video-amdgpu ) vdriver="xf86-video-amdgpu vulkan-radeon"; break;;
             2|xf86-video-ati ) vdriver=xf86-video-ati; break;;
             3|xf86-video-intel ) vdriver=xf86-video-intel; break;;
             4|nvidia ) vdriver=nvidia; break;;
-            * ) printf "Invalid input.\n\n";;
+            * ) vdriver=""; printf "Invalid input.\n\n";;
         esac
     done
 
     printf "Installing the Gnome preset\n"
+    # shellcheck disable=SC2086 # because 'vdriver' can be empty and we don't want pacstrap to fail
     pacstrap /mnt $vdriver gnome cups unrar vim firefox transmission-gtk rhythmbox thunderbird steam mpv libreoffice hplip keepassxc gparted ttf-dejavu noto-fonts-cjk neofetch ghex gnome-software-packagekit-plugin bat fzf chafa
 
     printf "Enabling services for the Gnome preset\n"
@@ -345,14 +388,17 @@ install_preset() {
     # init here hplip
     # install youtube-dl with python-pip
 
-    # AUR
-    arch-chroot /mnt /bin/bash << EOF
-    printf "Enabling access to the AUR"
-    git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
+    # # AUR
+    # # TODO this fails because we can't use makepkg as root
+    # arch-chroot -u maxime /mnt /bin/bash << EOF
+    # printf "Enabling access to the AUR\n"
+    # pacman --noconfirm -S go
+    # pacman -D --asdeps go
+    # git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay
 
-    printf "Installing the AUR packages"
-    yay -S --noconfirm chrome-gnome-shell megasync-bin nautilus-megasync rhythmbox-plugin-alternative-toolbar ttf-ms-fonts visual-studio-code-bin nautilus-admin-git corectrl
-EOF
+    # printf "Installing the AUR packages"
+    # yay -S --noconfirm chrome-gnome-shell megasync-bin nautilus-megasync rhythmbox-plugin-alternative-toolbar ttf-ms-fonts visual-studio-code-bin nautilus-admin-git corectrl
+# EOF
 }
 
 epilogue() {
@@ -360,7 +406,42 @@ epilogue() {
     exit # exit the chroot
 }
 
-# TODO test this as it is for now in a VM!!!
+# TODO adapt these functions
+# install_yay() {
+#     # exec command as user instead of root
+#     # test changing aui_packages to /tmp
+#     su - "$user" -c "
+#     [[ ! -d aui_packages ]] && mkdir aui_packages
+#     cd aui_packages
+#     curl -o ${PKG}.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/${PKG}.tar.gz
+#     tar zxvf ${PKG}.tar.gz
+#     rm ${PKG}.tar.gz
+#     cd ${PKG}
+#     makepkg -csi --noconfirm
+# "
+# }
+# aur_package_install() {
+# 	su - "$user" -c "sudo -v"
+# 	#install package from aur
+# 	for PKG in $1; do
+# 		if ! is_package_installed "${PKG}"; then
+# 			if [[ $AUTOMATIC_MODE -eq 1 ]]; then
+# 				ncecho " ${BBlue}[${Reset}${Bold}X${BBlue}]${Reset} Installing ${AUR} ${Bold}${PKG}${Reset} "
+# 				su - "${username}" -c "${AUR_PKG_MANAGER} --noconfirm -S ${PKG}" >>"$LOG" 2>&1 &
+# 				pid=$!
+# 				progress $pid
+# 			else
+# 				su - "${username}" -c "${AUR_PKG_MANAGER} --noconfirm -S ${PKG}"
+# 			fi
+# 		else
+# 			if [[ $VERBOSE_MODE -eq 0 ]]; then
+# 				cecho " ${BBlue}[${Reset}${Bold}X${BBlue}]${Reset} Installing ${AUR} ${Bold}${PKG}${Reset} success"
+# 			else
+# 				echo -e "Warning: ${PKG} is up to date --skipping"
+# 			fi
+# 		fi
+# 	done
+# }
 
 ##################################################
 
@@ -368,7 +449,7 @@ epilogue() {
 
 detect_efi
 next
-select_keyboard_layout
+ask_keyboard_layout
 next
 update_system_clock
 next
@@ -378,7 +459,13 @@ format_partitions
 next
 mount_filesystems
 next
+ask_grub
+next
 setup_swapfile
+next
+ask_hostname
+ask_root_password
+ask_username_and_password
 
 # INSTALL
 
@@ -388,9 +475,7 @@ next
 install_grub
 set_shell_timezone_clock_locales
 next
-ask_hostname
-ask_root_passwd
-create_user
+set_hostname_user_and_passwords
 next
 install_preset
 next
